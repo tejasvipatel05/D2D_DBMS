f8628b26e7fba2b450a0b94fc31df289858c62f1
Tejasvi Patel <tejasvipatel1010@gmail.com>
2025-08-19 21:58:59 +0530

    Lab 8 table creation, insertion

---
+++ Lab8_table.sql
@@ -0,0 +1,34 @@
+CREATE TABLE DEPT (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(100) NOT NULL UNIQUE,
+    DepartmentCode VARCHAR(50) NOT NULL UNIQUE,
+    Location VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE PERSON (
+    PersonID INT PRIMARY KEY,
+    PersonName VARCHAR(100) NOT NULL,
+    DepartmentID INT FOREIGN KEY REFERENCES DEPT(DepartmentID),
+    Salary DECIMAL(8,2) NOT NULL,
+    JoiningDate DATETIME NOT NULL,
+    City VARCHAR(100) NOT NULL
+);
+
+INSERT INTO DEPT VALUES
+(1, 'Admin', 'Adm', 'A-Block'),
+(2, 'Computer', 'CE', 'C-Block'),
+(3, 'Civil', 'CI', 'G-Block'),
+(4, 'Electrical', 'EE', 'E-Block'),
+(5, 'Mechanical', 'ME', 'B-Block');
+
+INSERT INTO PERSON VALUES
+(101, 'Rahul Tripathi', 2, 56000, '2000-01-01', 'Rajkot'),
+(102, 'Hardik Pandya', 3, 18000, '2001-09-25', 'Ahmedabad'),
+(103, 'Bhavin Kanani', 4, 25000, '2000-05-14', 'Baroda'),
+(104, 'Bhoomi Vaishnav', 1, 39000, '2005-02-08', 'Rajkot'),
+(105, 'Rohit Topiya', 2, 17000, '2001-07-23', 'Jamnagar'),
+(106, 'Priya Menpara', NULL, 9000, '2000-10-18', 'Ahmedabad'),
+(107, 'Neha Sharma', 2, 34000, '2002-12-25', 'Rajkot'),
+(108, 'Nayan Goswami', 3, 25000, '2001-07-01', 'Rajkot'),
+(109, 'Mehul Bhundiya', 4, 13500, '2005-01-09', 'Baroda'),
+(110, 'Mohit Maru', 5, 14000, '2000-05-25', 'Jamnagar');
