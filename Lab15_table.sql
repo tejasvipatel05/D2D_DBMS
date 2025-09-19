bf0cd1e2add7ebcf3c7ca83149dc1fd9be98ee40
Tejasvi Patel <tejasvipatel1010@gmail.com>
2025-09-19 11:02:07 +0530

    Lab 15 table creation, insertion queries

---
+++ Lab15_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE PersonInfo (
+    PersonID INT PRIMARY KEY,
+    PersonName VARCHAR(100) NOT NULL,
+    Salary DECIMAL(8,2) NOT NULL,
+    JoiningDate DATETIME NULL,
+    City VARCHAR(100) NOT NULL,
+    Age INT NULL,
+    BirthDate DATETIME NOT NULL
+);
+
+CREATE TABLE PersonLog (
+    PLogID INT PRIMARY KEY IDENTITY(1,1),
+    PersonID INT NOT NULL,
+    PersonName VARCHAR(250) NOT NULL,
+    Operation VARCHAR(50) NOT NULL,
+    UpdateDate DATETIME NOT NULL
+);
